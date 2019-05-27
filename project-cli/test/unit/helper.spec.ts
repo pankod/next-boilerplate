718f3dbe15a49130a6bb71f2f527f11a08416d2b
Tamer Durgun <tamer@pankod.com>
2019-05-27 15:48:13 +0300

    test file created

--- project-cli/test/unit/helper.spec.ts
+++ project-cli/test/unit/helper.spec.ts
@@ -1,1 +1,22 @@
 
+
+import { Config } from '../../config';
+import { DefinationsModel } from '../../src/definations/Defination';
+import { Helper } from '../../src/definations/helper';
+import * as path from 'path';
+
+describe('Test Helper constructor', () => {
+
+	 test('getTemplate method', () => {
+		const templatePath = `${Config.mockDir}/simpleText.mustache`;
+
+		const templateProps = {
+			fileName: 'collection'
+		};
+
+		const result = Helper.getTemplate(templatePath, templateProps);
+
+		expect(result).not.toEqual('');
+	});
+
+});
