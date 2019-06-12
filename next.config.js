4bea416a801111a2f59a2cd8ada1d0e4a404a368
Batuhan Ozgur Ozdemir <batuhan@pankod.com>
2019-06-12 17:10:46 +0300

    Disable TS rule for next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const withPlugins = require('next-compose-plugins');
 
 const withTypescript = require('@zeit/next-typescript');
@@ -5,6 +6,7 @@ const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 const nextRuntimeDotenv = require('next-runtime-dotenv');
+/* eslint-enable @typescript-eslint/no-var-requires */
 
 const withConfig = nextRuntimeDotenv({
 	public: ['API_URL', 'API_KEY'],
