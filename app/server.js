510cacfcb5bfdf2513a6793d03433278b834e7c9
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-08-23 13:40:44 +0300

    Refactor server.js for i18n

--- app/server.js
+++ app/server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const next = require('next');
 const path = require('path');
 const devProxy = require('./proxy.js');
+const i18nextMiddleware = require('i18next-express-middleware');
+const Backend = require('i18next-node-fs-backend');
+const { i18nInstance } = require('./i18n');
+const configI18next = require('./i18next.config');
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,28 +17,36 @@ const app = next({
 const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
-app.prepare().then(() => {
-	const server = express();
+i18nInstance
+	.use(Backend)
+	.use(i18nextMiddleware.LanguageDetector)
+	.init(configI18next.server, () => {
+		app.prepare().then(() => {
+			const server = express();
 
-	app.setAssetPrefix(process.env.STATIC_PATH);
+			app.setAssetPrefix(process.env.STATIC_PATH);
+			server.use(express.static(path.join(__dirname, '../static')));
+			server.use(i18nextMiddleware.handle(i18nInstance));
+			server.use('/locales', express.static(path.join(__dirname, '../locales')));
+			server.use(express.static(path.join(__dirname, '../static')));
+			server.post('/locales/add/:lng/:ns', i18nextMiddleware.missingKeyHandler(i18nInstance));;
 
-	server.use(express.static(path.join(__dirname, '../static')));
+			if (process.env.PROXY_MODE === 'local') {
+				const proxyMiddleware = require('http-proxy-middleware');
+				Object.keys(devProxy).forEach(function(context) {
+					server.use(proxyMiddleware(context, devProxy[context]));
+				});
+			}
 
-	if (process.env.PROXY_MODE === 'local') {
-		const proxyMiddleware = require('http-proxy-middleware');
-		Object.keys(devProxy).forEach(function(context) {
-			server.use(proxyMiddleware(context, devProxy[context]));
+			server.get('*', async (req, res) => {
+				return handler(req, res);
+			});
+
+			server.listen(PORT, err => {
+				if (err) throw err;
+				console.log(`> Ready on http://localhost:${PORT}`);
+			});
 		});
-	}
-
-	server.get('*', async (req, res) => {
-		return handler(req, res);
 	});
-
-	server.listen(PORT, err => {
-		if (err) throw err;
-		console.log(`> Ready on http://localhost:${PORT}`);
-	});
-});
 
 /* eslint-enable @typescript-eslint/no-var-requires */
