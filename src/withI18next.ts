50a1a9bca62fa92d77747d3c32f1c39dff3e5bce
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-08-23 13:40:09 +0300

    withI18next HOC

---
+++ src/withI18next.ts
@@ -0,0 +1,25 @@
+import { translate } from 'react-i18next';
+import { getInitialProps, I18n } from '../app/i18n';
+
+export const withI18next = (namespaces = ['common', 'search']) => ComposedComponent => {
+	const Extended = translate(namespaces, { i18n: I18n, wait: process.browser })(
+		ComposedComponent
+	);
+
+	Extended.getInitialProps = async ctx => {
+
+		const composedInitialProps = ComposedComponent.getInitialProps
+			? await ComposedComponent.getInitialProps(ctx)
+			: {};
+
+		const i18nInitialProps =
+			ctx.req && !process.browser ? getInitialProps(ctx.req, namespaces) : {}
+
+		return {
+			...composedInitialProps,
+			...i18nInitialProps,
+		};
+	};
+
+	return Extended;
+};
