5522907f802326fc773c8db35fb8a1b945438cde
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-08-23 13:58:40 +0300

    Refactor withI18Next HOC

--- src/withI18next.ts
+++ src/withI18next.ts
@@ -1,16 +1,19 @@
 import { translate } from 'react-i18next';
 import { getInitialProps, I18n } from '../app/i18n';
+import { NextComponentType, NextContext, NextComponentClass } from 'next';
 
-export const withI18next = (namespaces = ['common', 'search']) => ComposedComponent => {
-	const Extended = translate(namespaces, { i18n: I18n, wait: process.browser })(
+const setComposedInitialProps = async (ComposedComponent: NextComponentType, ctx: NextContext) => {
+	return ComposedComponent.getInitialProps ? await ComposedComponent.getInitialProps(ctx) : {}
+}
+
+export const withI18next = (namespaces = ['common']) => (ComposedComponent: NextComponentType) => {
+	const Extended: NextComponentClass = translate(namespaces, { i18n: I18n, wait: process.browser })(
 		ComposedComponent
 	);
 
-	Extended.getInitialProps = async ctx => {
+	Extended.getInitialProps = async (ctx: NextContext) => {
 
-		const composedInitialProps = ComposedComponent.getInitialProps
-			? await ComposedComponent.getInitialProps(ctx)
-			: {};
+		const composedInitialProps = await setComposedInitialProps(ComposedComponent, ctx)
 
 		const i18nInitialProps =
 			ctx.req && !process.browser ? getInitialProps(ctx.req, namespaces) : {}
