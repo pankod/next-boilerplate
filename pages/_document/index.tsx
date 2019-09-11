1c135167dad61f98eead3cfdf7fc8a34225ce374
burak <burak@buraks-MacBook-Pro.local>
2019-09-11 18:43:34 +0300

    Add ssr styling codes to _document

--- pages/_document/index.tsx
+++ pages/_document/index.tsx
@@ -6,11 +6,30 @@ import Document, {
     NextScript,
     DocumentContext,
 } from "next/document";
+import { ServerStyleSheet } from 'styled-components';
 
 class WebAppDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+
+        try {
+            ctx.renderPage = () => originalRenderPage({
+                enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+            });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps, styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                )
+            };
+        } finally {
+            sheet.seal();
+        }
     }
 
     render() {
