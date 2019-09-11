665e1bd98a9eee1bc29c301b616f36ce5fa0576a
burak <burak@buraks-MacBook-Pro.local>
2019-09-11 18:44:15 +0300

    Convert styling from scss to styled-components

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,14 +1,100 @@
 import * as React from "react";
 import { NextPage, NextPageContext } from "next";
 import { useSelector, useDispatch } from "react-redux";
+import styled from "styled-components";
 
 import { withTranslation } from "@Server/i18n";
 
 import { IHomePage } from "./Home";
 
-import "./index.scss";
+//import "./index.scss";
 import { IStore } from "@Redux/IStore";
 import { HomeActions } from "@Actions";
+
+const Container = styled.div`
+    background-color: #2c3e50;
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 100%;
+    justify-content: flex-start;
+    align-items: center;
+    min-height: 100vh;
+`;
+
+const ContainerTop = styled.div`
+    margin: 100px;
+`;
+
+const ContainerMiddle = styled.div`
+    text-align: center;
+    display: flex;
+    flex: 1 1 100%;
+    width: 100%;
+    justify-content: flex-start;
+`;
+
+const ContainerMiddleLeft = styled.div`
+    display: flex;
+    flex: 1 1 1%;
+`;
+
+const ContainerMiddleLeftButtons = styled.div`
+    background: #f9da2e;
+    border-radius: 8px;
+    font-family: Arial;
+    font-weight: 700;
+    font-size: 25px;
+    color: #4b6c8d;
+    letter-spacing: 0;
+    width: 55px;
+    height: 236px;
+    margin-left: 15px;
+    padding: 10px 3px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    color: #4b6c8d;
+
+    .button {
+        cursor: pointer;
+    }
+
+    .active {
+        color: #2c3e50;
+    }
+`;
+
+const ContainerMiddleRight = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 100%;
+`;
+
+const ContainerMiddleRightApod = styled.div`
+    display: flex;
+    flex: 1 1 100%;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+`;
+
+const ContainerMiddleRightApodButton = styled.div`
+    background: #f9da2e;
+    border-radius: 5px;
+    padding: 5px 10px;
+    font-family: Arial;
+    font-weight: 700;
+    font-size: 25px;
+    color: #2c3e50;
+    letter-spacing: 0;
+`;
+
+const ContainerTopText = styled.div`
+    font-size: 110px;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #ffffff;
+    margin-bottom: 10px;
+`;
 
 const Home: NextPage<IHomePage.IProps, IHomePage.InitialProps> = ({
     t,
@@ -23,7 +109,7 @@ const Home: NextPage<IHomePage.IProps, IHomePage.InitialProps> = ({
                 key={lang}
                 className={`button ${lang} ${
                     activeLanguage === lang ? "active" : ""
-                }`}
+                    }`}
                 onClick={() => i18n.changeLanguage(lang)}
             >
                 {lang}
@@ -31,24 +117,23 @@ const Home: NextPage<IHomePage.IProps, IHomePage.InitialProps> = ({
         ));
 
     return (
-        <div className="container">
-            <div className="container__top">
+        <Container>
+            <ContainerTop>
                 <img src="/static/images/pankod-logo.png" alt="Pankod Logo" />
-            </div>
-            <div className="container__middle">
-                <div className="container__middle__left">
-                    <div className="container__middle__left__buttons">
+            </ContainerTop>
+            <ContainerMiddle>
+                <ContainerMiddleLeft>
+                    <ContainerMiddleLeftButtons>
                         {renderLocaleButtons(i18n.language)}
-                    </div>
-                </div>
-                <div className="container__middle__right">
-                    <span className="container__top_text">
+                    </ContainerMiddleLeftButtons>
+                </ContainerMiddleLeft>
+                <ContainerMiddleRight>
+                    <ContainerTopText>
                         {t("common:Hello")}
-                    </span>
+                    </ContainerTopText>
                     {/* <Heading text={t("common:World")} /> */}
-                    <span className="container__middle__right__apod">
-                        <span
-                            className="container__middle__right__apod__button"
+                    <ContainerMiddleRightApod>
+                        <ContainerMiddleRightApodButton
                             onClick={() => {
                                 dispatch(
                                     HomeActions.GetApod({
@@ -58,17 +143,17 @@ const Home: NextPage<IHomePage.IProps, IHomePage.InitialProps> = ({
                             }}
                         >
                             Get A Photo
-                        </span>
+                        </ContainerMiddleRightApodButton>
                         <img
                             src={home.image.url}
                             height="300"
                             width="150"
                             alt="APOD"
                         />
-                    </span>
-                </div>
-            </div>
-        </div>
+                    </ContainerMiddleRightApod>
+                </ContainerMiddleRight>
+            </ContainerMiddle>
+        </Container>
     );
 };
 
