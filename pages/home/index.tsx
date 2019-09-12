68d2daec2dc8af4a6965547e57090adfeafa2e1d
Omer Faruk APLAK <omeraplak@gmail.com>
2019-09-12 17:51:14 +0300

    update apod button text

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -33,6 +33,8 @@ const Home: NextPage<IHomePage.IProps, IHomePage.InitialProps> = ({
     const home = useSelector((state: IStore) => state.home);
     const dispatch = useDispatch();
 
+    console.log(i18n)
+
     const renderLocaleButtons = (activeLanguage: string) =>
         ["en", "es", "tr"].map(lang => (
             <LocaleButton
@@ -67,13 +69,13 @@ const Home: NextPage<IHomePage.IProps, IHomePage.InitialProps> = ({
                                 );
                             }}
                         >
-                            Get A Photo
+                            Discover Space
                         </ApodButton>
                         <img
                             src={home.image.url}
                             height="300"
                             width="150"
-                            alt="APOD"
+                            alt="Discover Space"
                         />
                     </Apod>
                 </MiddleRight>
