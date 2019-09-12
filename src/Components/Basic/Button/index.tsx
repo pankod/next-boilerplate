20083b1f9ddd7555ee41aec0f8f5a687c447dc8a
Burak Kaplan <burak@kaplan.dev>
2019-09-12 15:19:06 +0300

    Add Basic>Button component

---
+++ src/Components/Basic/Button/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { IButton } from './Button';
+
+const StyledDiv = styled.div`
+    cursor: pointer;
+    
+    .active {
+        color: #2c3e50;
+    }
+`;
+
+export const Button: React.FunctionComponent<IButton.IProps> = ({ children, onClick, className }) => {
+    return <StyledDiv className={className} onClick={onClick}>{children}</StyledDiv>;
+};
+
