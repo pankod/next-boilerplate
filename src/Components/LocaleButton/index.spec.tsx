92aeb85187f3ccb33e8a148dfc656fb5020f6212
Burak Kaplan <burak@kaplan.dev>
2019-09-12 15:57:53 +0300

    Add test to cover 100% of LocaleButton

--- src/Components/LocaleButton/index.spec.tsx
+++ src/Components/LocaleButton/index.spec.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { LocaleButton } from './index';
+import React from "react";
+import { mount } from "enzyme";
+import { LocaleButton } from "./index";
 
 describe("Components", () => {
     describe("LocaleButton", () => {
+        it("should render without throwing error", () => {
+            const wrapper = mount(
+                <LocaleButton lang="tr" isActive={false} onClick={() => true} />
+            );
+            expect(wrapper.find("div").length).toBe(1);
+        });
+
         let number = 1;
-        const wrapper = mount(<LocaleButton lang="tr" isActive={true} onClick={() => number++} />);
+        const wrapper = mount(
+            <LocaleButton lang="tr" isActive={true} onClick={() => number++} />
+        );
 
         it("should render without throwing error", () => {
-            expect(wrapper.find('div.active').exists()).toBe(true);
+            expect(wrapper.find("div.active").exists()).toBe(true);
         });
 
         it("should increment number on click", () => {
-            wrapper.simulate('click');
+            wrapper.simulate("click");
             expect(number).toBe(2);
         });
 
         it("should render Button with lang", () => {
-            expect(wrapper.childAt(0).props().children).toBe('tr');
+            expect(wrapper.childAt(0).props().children).toBe("tr");
         });
     });
 });
