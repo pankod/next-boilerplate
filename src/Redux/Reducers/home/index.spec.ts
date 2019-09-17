c9f37dd56a1490a3cce8677ab7956c6d1caa9289
Burak Kaplan <burak@kaplan.dev>
2019-09-17 10:42:24 +0300

    Fix types errors in home spec

--- src/Redux/Reducers/home/index.spec.ts
+++ src/Redux/Reducers/home/index.spec.ts
@@ -7,7 +7,7 @@ import { HomeReducer } from ".";
 describe("home reducer", () => {
     it("should return the initial state", () => {
         expect(
-            HomeReducer(undefined, {} as IAction<IHomePage.IDispatchProps>)
+            HomeReducer(undefined, {} as IAction<IHomePage.IStateProps>)
         ).toEqual({
             home: {
                 version: 1,
@@ -20,7 +20,7 @@ describe("home reducer", () => {
 
     it("should handle SetReducer", () => {
         expect(
-            HomeReducer([], {
+            HomeReducer(undefined, {
                 type: ActionConsts.Home.SetReducer,
                 payload: {
                     version: 2,
@@ -33,7 +33,7 @@ describe("home reducer", () => {
 
     it("should handle ResetReducer", () => {
         expect(
-            HomeReducer([], {
+            HomeReducer(undefined, {
                 type: ActionConsts.Home.ResetReducer,
             })
         ).toEqual({
