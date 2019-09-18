27db3091b8ad8932fee35ed8810282d36a275ad3
Burak Kaplan <burak@kaplan.dev>
2019-09-18 15:36:09 +0300

    Add moduleNameMapper for test helpers

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,7 @@ module.exports = {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
             "<rootDir>/test/mocks.ts",
         "\\.(css|less|scss|html)$": "<rootDir>/test/mocks.ts",
+        "^@(Test)(.*)$": "<rootDir>/test/$2",
         "^@([A-Z].*)$": "<rootDir>/src/$1",
     },
     preset: "ts-jest",
