ecaa12b847f9e2028c221ff286dd25081899301a
Burak Kaplan <burak@kaplan.dev>
2019-09-19 12:18:35 +0300

    Use theme for color value

--- src/Components/Basic/Button/index.tsx
+++ src/Components/Basic/Button/index.tsx
@@ -9,6 +9,7 @@ import { IButton } from "./Button";
 
 const Container = styled.div<IButton.IProps>`
     cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+    color: ${({ theme }) => theme.colors.primary};
 `;
 
 export const Button: React.FunctionComponent<IButton.IProps> = props => {
