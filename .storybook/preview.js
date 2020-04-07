3c1028d3473a2da0dbe14fdbd502378cad292dc5
Muharrem <mkocadere@gmail.com>
2020-04-07 13:48:37 +0300

    fix typo

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,7 +6,7 @@ import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 // #endregion Global Imports
 
 // #region Local Imports
-import {theme} from "@Defintions/Styled"
+import {theme} from "@Definitions/Styled"
 import {withRedux, withI18next} from "./Decorators"
 // #endregion Local Imports
 
