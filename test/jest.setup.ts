72cfac7094ce67df29f6cabdf739afb05de32a92
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2020-05-01 12:04:05 +0300

    Setup for testing library

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,13 +1,10 @@
 // #region Global Imports
 import dotenv from "dotenv";
-import enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
 import nock from "nock";
 import { setConfig } from "next/config";
 import "jest-styled-components";
+import "@testing-library/jest-dom";
 // #endregion Global Imports
-
-enzyme.configure({ adapter: new Adapter() });
 
 dotenv.config({ path: ".env.test" });
 
