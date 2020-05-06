8acebc337fc3954421ca7669aba1e9a59bd8fcb2
Omer Faruk APLAK <omeraplak@gmail.com>
2020-05-06 17:11:51 +0300

    remove unnecessary testMatch option in jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
-    testMatch: ["**/*.(test|spec).(ts|tsx)"],
     globals: {
         "ts-jest": {
             tsConfig: "tsconfig.jest.json",
