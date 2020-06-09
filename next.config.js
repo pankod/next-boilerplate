a993e9a7eb1baf38b7ba0564db26df9738673a08
Ali Emir Şen <senaliemir@gmail.com>
2020-06-09 11:47:39 +0300

    replace deprecated zeit/next-bundle-analyzer to next/bundle-analyzer, Resolves #103 (#106)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withPlugins = require("next-compose-plugins");
 const withCSS = require("@zeit/next-css");
 const withSass = require("@zeit/next-sass");
-const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
+const withBundleAnalyzer = require("@next/bundle-analyzer");
 const nextRuntimeDotenv = require("next-runtime-dotenv");
 
 const withConfig = nextRuntimeDotenv({ public: ["API_URL", "API_KEY"] });
