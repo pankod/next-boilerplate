60d2d0675dd83af00e4e18d02385314fa3b19d22
Mehmet Sefa <mehmetsefabalik@gmail.com>
2020-08-03 18:20:17 +0300

    Update health-check.ts

--- pages/api/health-check.ts
+++ pages/api/health-check.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse): any => {
-    res.status(200).json({ status: "working" });
+    res.status(200).json({ status: "OK" });
 };
