8085e00d4b3d435225e666618309404d7d9db4ef
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2020-10-04 19:07:13 +0300

    Format js files

--- next.config.js
+++ next.config.js
@@ -2,18 +2,18 @@ const withPlugins = require("next-compose-plugins");
 const withBundleAnalyzer = require("@next/bundle-analyzer");
 
 const nextConfig = {
-    analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-    analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
-    bundleAnalyzerConfig: {
-        server: {
-            analyzerMode: "static",
-            reportFilename: "../bundles/server.html",
-        },
-        browser: {
-            analyzerMode: "static",
-            reportFilename: "../bundles/client.html",
-        },
+  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/server.html",
     },
+    browser: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/client.html",
+    },
+  },
 };
 
 module.exports = withPlugins([[withBundleAnalyzer]], nextConfig);
